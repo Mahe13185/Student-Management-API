79a2b9b60fb203527b12b2f147c39c8c14e8e6b4
Mahendra Gampala <mahendragampala13185@gmail.com>
2026-07-06 17:53:49 +0530

    initial commit

---
+++ src/test/java/org/mahendra/studentmanagementapi/StudentManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mahendra.studentmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
